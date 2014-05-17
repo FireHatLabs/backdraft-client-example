bf9d0c1cacf7afc07e93e220532314a94643c9b7
Chris Compton <chris@chriscompton.me>
2014-05-17 08:07:42 -0500

    Added comment relationship

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -36,21 +36,41 @@ BDApp.ItemsRoute = Ember.Route.extend({
   }  
 });
 
-
 BDApp.Item = DS.Model.extend({
   title: DS.attr('string'),
-  description: DS.attr('string')
+  description: DS.attr('string'),
+  comments: DS.hasMany('comment', { async: true })
+});
+
+BDApp.Comment = DS.Model.extend({
+  content: DS.attr('string'),
+  addedAt: DS.attr('date'),
+  item: DS.belongsTo('item')
 });
 
 BDApp.Item.FIXTURES = [
   {
     id: 1,
     title: "One",
-    description: "Test Item"
+    description: "Test Item",
+    comments: [11,12]
   },
   {
     id: 2,
     title: "Two",
     description: "Another Test Item"
   }    
+];
+
+BDApp.Comment.FIXTURES = [
+  {
+    id: 11,
+    item: 1,
+    content: "Another Test Comment"
+  },
+  {
+    id: 12,
+    item: 1,
+    content: "Test Comment"
+  }
 ];
