c46a96c33fb161686d6e9061a73c068570d43daf
Chris Compton <chris@chriscompton.me>
2014-05-17 00:35:08 -0500

    Adjusting routes.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -6,6 +6,9 @@ bdApp.Router.map(function() {
   this.route('register');
   this.route('login');
   this.route('account');
+    
+  this.resource('items');
+  this.resource('item',  { path: 'items/:title' });
 });
 
 bdApp.IndexRoute = Ember.Route.extend({
@@ -18,3 +21,29 @@ bdApp.AccountController = Ember.Controller.extend({
   firstName: 'Bobby',
   lastName: 'Tables'
 });
+
+
+// Items ////////////////////////////////
+
+bdApp.ItemsRoute = Ember.Route.extend({
+  model: function() {
+    return bdApp.ITEMS;
+  }  
+});
+
+bdApp.ItemRoute = Ember.Route.extend({
+  model: function(params) {
+    return bdApp.ITEMS.findBy('title', params.title);
+  }
+});
+
+bdApp.ITEMS = [
+  {
+    title: "One",
+    description: "Test Item"
+  },
+  {
+    title: "Two",
+    description: "Another Test Item"
+  }    
+];
