215de4a753bd8c3f04c6324bab50653e89d2481f
Chris Compton <chris@chriscompton.me>
2014-05-19 22:05:09 -0500

    Working on login

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -9,3 +9,28 @@ BDApp.AccountController = Ember.ArrayController.extend({
   lastName: 'Tables',
   itemsCount: Ember.computed.alias('length')
 });
+
+BDApp.LoginController = Ember.ArrayController.extend({
+  username: 'Bobby',
+  password: 'Tables',
+  isAuthenticated: false,
+  actions: {
+    login: function() {
+      console.log('login called');
+    },
+    logout: function() {
+      console.log('logout called');
+    }
+  }
+});
+
+BDApp.RegistrationController = Ember.ArrayController.extend({
+  template: 'register',
+  username: 'Bobby',
+  password: 'Tables',
+  actions: {
+    register: function() {
+      console.log('register called');
+    }
+  }
+});
