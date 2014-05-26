5f23097a2200f012fdd4d65bf0f40c7ea0bd5b44
Chris Compton <chris@chriscompton.me>
2014-05-26 08:42:57 -0500

    Move templates to app.js

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,3 +5,16 @@ var BDApp = Ember.Application.create({
 BDApp.ApplicationSerializer = DS.ActiveModelSerializer.extend({
   primaryKey: '_id'
 });
+
+function loadTemplate(name) {
+  return $.get('hb/'+name+'.hb').then(function(src) {
+    $("body").prepend(src);
+  });
+}
+
+loadTemplate('nav');
+loadTemplate('index');
+loadTemplate('login');
+loadTemplate('registration');
+loadTemplate('account');
+loadTemplate('item');
